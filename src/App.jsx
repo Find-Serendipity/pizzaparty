9a12fb8c3fe051cf6b1246cf11a6749a7a148827
Find-Serendipity <find.serendipity@gmail.com>
2024-03-28 12:52:26 -0400

    basic connection established

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <h1>What pizza would you like to order?</h1>
+    </>
+  );
 }
 
 export default App;
